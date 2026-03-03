0721cbdec6b63ff92eb855c37b9867f21ff6136d
SuprimOjha <suprimojha25@gmail.com>
2026-03-03 22:27:39 +0545

    Deleted Lib

--- lib/api.ts
+++
@@ -1,8 +0,0 @@
-// src/lib/api.ts
-const API_URL = "http://localhost:1337/api";
-
-export async function fetchAPI(path: string) {
-  const res = await fetch(`${API_URL}${path}`);
-  const data = await res.json();
-  return data.data;
-}
